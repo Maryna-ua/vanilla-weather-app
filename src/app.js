78431b2d30bb6faf50b56e9614602f94e81fb2b5
Marina <kodola.marina@gmail.com>
2022-07-21 08:58:43 +0300

    display current weather

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-alert("👋🏼HI!");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = `${response.data.name}, `;
+
+  let countryElement = document.querySelector("#country");
+  countryElement.innerHTML = response.data.sys.country;
+
+  let skyElement = document.querySelector("#sky");
+  skyElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "1c6f613839ce6b71f2df6e20804a91d8";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=kyiv&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
