d85b1799ab899dbb5b85022aaf74c16cd9c0157d
Marina <kodola.marina@gmail.com>
2022-07-21 14:34:31 +0300

    Updated the date and  time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+
+  if (hours < 10) {
+    minutes = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temp");
@@ -17,9 +34,12 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateEement = document.querySelector("#date");
+  dateEement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "1c6f613839ce6b71f2df6e20804a91d8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=kyiv&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
